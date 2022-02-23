a4f1a8c06a0a6c1d64f69dfa3cdbf788c04ebdfc
hiddy0329 <hiddy19970329@gmail.com>
2022-02-23 19:35:51 +0900

    ヘルパーメソッドの作成

--- app/helpers/slips_helper.rb
+++ app/helpers/slips_helper.rb
@@ -1,2 +1,9 @@
 module SlipsHelper
+  def slip_lists(slips)
+    html = ''
+    slips.each do |slip|
+      html += render(partial: "slip", locals: { slip: slip }) 
+    end
+    return raw(html)
+  end
 end
