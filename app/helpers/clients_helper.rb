0691c07a9b6f774846a1cd7cbaf079a43a5a9f11
hiddy0329 <hiddy19970329@gmail.com>
2022-02-24 17:24:09 +0900

    繰り返し処理のヘルパーメソッドへの書き出し

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,2 +1,9 @@
 module ClientsHelper
+  def client_lists(clients)
+    html = ''
+    clients.each do |client|
+      html += render(partial: "client", locals: { client: client }) 
+    end
+    return raw(html)
+  end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+  def product_lists(products)
+    html = ''
+    products.each do |product|
+      html += render(partial: "product", locals: { product: product }) 
+    end
+    return raw(html)
+  end
 end
