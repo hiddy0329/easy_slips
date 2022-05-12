5b7406ffc7ff43bb4637c69e9f7fdf1754e2641b
hiddy0329 <hiddy19970329@gmail.com>
2022-05-12 17:17:02 +0900

    「order_number」の一意性制約を排除

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,4 @@
 class Order < ApplicationRecord
-  validates :order_number,  uniqueness: true
   validates :note, length: { maximum: 20 }
 
   belongs_to :slip
--- db/migrate/20220204105625_create_orders.rb
+++ db/migrate/20220204105625_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.integer :order_number, uniqueness: true
+      t.integer :order_number
       t.string :color
       t.integer :count
       t.string :note, limit: 20
