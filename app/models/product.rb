5bcf9b3040c4ae4c0f9777559b92292f8d42e678
hiddy0329 <hiddy19970329@gmail.com>
2022-05-16 15:02:08 +0900

    Productモデルのバリデーションを修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,8 @@
 class Product < ApplicationRecord
   with_options presence: true do
     validates :item_number, length: { maximum: 10 }
-    validates :name, length: { maximum: 20 }
     validates :color
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :note, length: { maximum: 20 }
     validates :availability, inclusion: { in: [true, false] }
   end
-
-  validates :note, length: { maximum: 20 }
-
 end
